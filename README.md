447bd6a0be943c1b2f733fd2af0b6ec8d1a6c957
Athul Cyriac Ajay <athul8720@gmail.com>
2020-09-20 23:41:23 +0530

    Updated Scripts and readme

---
+++ README.md
@@ -0,0 +1,3 @@
+# Notes
+
+These are my personal notes which I jot down for educational and other purposes :grin:
--- build-zets.sh
+++ build-zets.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 echo "Downloading Zettel"
-curl -sL "https://github.com/hackstream/zettel/releases/download/v0.4.0/zettel_0.4.0_$(uname)_amd64.tar.gz" | tar xz
+curl -sL "https://github.com/hackstream/zettel/releases/download/v0.5.0/zettel_0.5.0_$(uname)_amd64.tar.gz" | tar xz
 echo "Zettel Downloaded"
 ./zettel build
