81d4064c328968955d1731d40fe43bc655d5c525
Athul Cyriac Ajay <athul8720@gmail.com>
2020-11-01 12:34:36 +0530

    zettel version changed

--- build-zets.sh
+++ build-zets.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 echo "Downloading Zettel"
-curl -sL "https://github.com/athul/zettel/releases/download/0.5.6/zettel_0.5.6_linux_amd64.tar.gz" | tar xz
+curl -sL "https://github.com/athul/zettel/releases/download/0.5.7/zettel_0.5.7_linux_amd64.tar.gz" | tar xz
 echo "Zettel Downloaded"
 ./zettel build
