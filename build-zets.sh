99a9785bab32a6cc589f994fdce6764a6b06f90a
Athul Cyriac Ajay <athul8720@gmail.com>
2020-11-01 21:45:36 +0530

    Changed to zet

--- build-zets.sh
+++ build-zets.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 echo "Downloading Zettel"
-curl -sL "https://github.com/athul/zettel/releases/download/0.5.7/zettel_0.5.7_linux_amd64.tar.gz" | tar xz
+curl -L "https://github.com/athul/zet/releases/download/0.1.0/zet_0.1.0_linux_amd64.tar.gz" | tar xz
 echo "Zettel Downloaded"
 ./zettel build
