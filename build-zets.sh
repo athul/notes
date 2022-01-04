bc6028f1d063d2a52cea0826edfadcf1c86b0245
Athul Cyriac Ajay <athul8720@gmail.com>
2022-01-04 22:44:48 +0530

    Update build-zets.sh

--- build-zets.sh
+++ build-zets.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 echo "Downloading Zettel"
-curl -L "https://github.com/athul/zet/releases/download/0.1.5/zet_0.1.5_linux_amd64.tar.gz" | tar xz
+curl -L "https://github.com/athul/zet/releases/download/0.1.6/zet_0.1.6_linux_amd64.tar.gz" | tar xz
 echo "Zettel Downloaded"
 ./zettel build
